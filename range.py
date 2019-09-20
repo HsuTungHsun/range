28f936f2c8ed6eb8eb1ce064459e1bad8309c497
Thomas Hsu <th30845@gmail.com>
2019-09-20 21:18:41 +0800

    for & range & random

--- range.py
+++ range.py
@@ -1,5 +1,6 @@
 # range
 #python function :list generator
+import random
 
 range(5) #[0, 1, 2, 3, 4]
 range(3) #[0, 1, 2, 3]
@@ -9,4 +10,5 @@ for i in range(3):
 			#print i =0
 			#print i =1
 			#print i =2
-	print('hi') #print 'hi' 3 times
+	r = random.randint(1, 1000)
+	print(r) #print 'hi' 3 times
