490622307d52abd4ef764fe28e7bf2596e62a17a
Thomas Hsu <th30845@gmail.com>
2019-09-20 21:50:31 +0800

    range advance

--- range.py
+++ range.py
@@ -5,12 +5,23 @@ import random
 range(5) #[0, 1, 2, 3, 4]
 range(3) #[0, 1, 2, 3]
 
-r = random.randint(1, 1000)
-for i in range(r):
+for i in range(100):
+	r = random.randint(1, 1000)
 	#print(i)
 			#print i =0
 			#print i =1
 			#print i =2
 	
 	print('hi') #print random times of 'hi'
-print(r)
+	print('This is: ', i + 1, 'times', r)
+
+#range advance
+
+#range(value)
+#range(start,end)
+#range(start,end,step)
+
+#range(3) 	[0, 1, 2]
+#range(2, 5) [2, 3, 4]
+#range(2, 10, 3) [2, 5, 8]
+#range(10, 3 , -2) [10, 8, 6, 4]
