2cc126c40cb1082d4ba71b1246d037d88180d3f2
Thomas Hsu <th30845@gmail.com>
2019-09-20 21:22:08 +0800

    print random times of 'hi'

--- range.py
+++ range.py
@@ -5,10 +5,12 @@ import random
 range(5) #[0, 1, 2, 3, 4]
 range(3) #[0, 1, 2, 3]
 
-for i in range(3):
+r = random.randint(1, 1000)
+for i in range(r):
 	#print(i)
 			#print i =0
 			#print i =1
 			#print i =2
-	r = random.randint(1, 1000)
-	print(r) #print 'hi' 3 times
+	
+	print('hi') #print random times of 'hi'
+print(r)
